a54b2c747bda2423b12f53ab610d340fae4e0e73
Valeria <valeria89_07@mail.ru>
2022-12-07 14:52:14 +0300

    задача 2

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// задача 2. принимает 2 числа и выдает какое больше, какое меньше
+Console.WriteLine("введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число B: ");
+int B = int.Parse(Console.ReadLine());
+if (A > B)
+{
+    Console.WriteLine($"число A -{A} -> max");
+}
+if (B > A)
+{
+    Console.WriteLine($"число B -{B} -> max");
+}
