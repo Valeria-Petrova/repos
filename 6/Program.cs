1d1b81bf5606897d0a6bfb4531e05d3165f3e0ae
Valeria <valeria89_07@mail.ru>
2022-12-07 14:55:20 +0300

    задача 6

---
+++ 6/Program.cs
@@ -0,0 +1,14 @@
+﻿//задача 6. на вход принимает число и выдает, является ли оно четным 
+
+Console.WriteLine("введите число A: ");
+int A = int.Parse(Console.ReadLine());
+
+
+if (A%2 == 0)
+{
+    Console.WriteLine($"Число {A} - четное");
+}
+else
+{
+    Console.WriteLine($"Число {A} - нечетное");
+}
