dbf68f798abe20db13543d6fc24443d89a54bc20
Valeria <valeria89_07@mail.ru>
2022-12-07 14:57:41 +0300

    задача 15

---
+++ 15/Program.cs
@@ -0,0 +1,18 @@
+﻿// задача 15. принимает цифру обозначающую день недели, и проверяет, явл. ли этот день выходным
+// 6 -> да 7 ->да 1 -> нет
+
+Console.WriteLine("Введите номер недели: ");
+int numberDayOfWeek = int.Parse(Console.ReadLine());
+if(numberDayOfWeek < 1 || numberDayOfWeek > 7)
+{
+    Console.WriteLine("Введен неправильный номер");
+    return;
+}
+if(numberDayOfWeek == 1 || numberDayOfWeek == 2 || numberDayOfWeek == 3 || numberDayOfWeek == 4 || numberDayOfWeek == 5)
+{
+    Console.WriteLine("нет");
+}
+if(numberDayOfWeek == 6 || numberDayOfWeek == 7)
+{
+    Console.WriteLine("да");
+}
