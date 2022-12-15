12d253b5ccb974707ea3b6e6a44d0b4c0ffbb2cc
Valeria <valeria89_07@mail.ru>
2022-12-15 11:13:13 +0300

    задача 8

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 8. число N и на выходе показывает все четные числа от 1 до N
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int A = 1;
+
+while (A <= N)
+{
+    if (A%2 == 0)
+    Console.WriteLine($"{A} ");
+    A++;
+}
