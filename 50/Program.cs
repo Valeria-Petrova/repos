cf085d94015bb7bd3fc7a06c5900d689b437de9b
Valeria <valeria89_07@mail.ru>
2022-12-17 12:37:55 +0300

    задача 50

---
+++ 50/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4      [1,7] -> такого числа в массиве нет
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+WriteLine();
+
+
+void NumberPosition (int [,] array, int i, int j)
+{
+    int result =0;
+            
+            if (i < array.GetLength(0) && j < array.GetLength(1))
+            {
+                WriteLine($"Значение элемента на позиции [{i},{j}] равно {array[i,j]}");
+            }
+            else {WriteLine("Нет элемента с таким индексом");
+            }
+        
+    
+}
+
+NumberPosition(array, 1, 7);
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
